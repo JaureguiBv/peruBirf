7971d75005d31c6e636408fc1d198360ff64a86b
REYNOSO JAUREGUI Gerardo <167136419+JaureguiBv@users.noreply.github.com>
2024-12-13 12:18:28 -0500

    Delete hospital directory

--- hospital/assets/script.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.delete').forEach(button => {
-    button.addEventListener('click', function (event) {
-        if (!confirm("¿Está seguro de que desea eliminar esta persona?")) {
-            event.preventDefault();
-        }
-    });
-});

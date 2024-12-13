d3a68a9feef7adf89d2bd3a639431a341d17b135
REYNOSO JAUREGUI Gerardo <167136419+JaureguiBv@users.noreply.github.com>
2024-12-13 12:19:00 -0500

    Delete Sistema_delegado directory

--- Sistema_delegado/js/validarcontraseña.js
+++
@@ -1,8 +0,0 @@
-const passwordInput = document.getElementById('password');
-const togglePasswordButton = document.getElementById('togglePassword');
-
-togglePasswordButton.addEventListener('click', () => {
-    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
-    passwordInput.setAttribute('type', type);
-    togglePasswordButton.innerHTML = type === 'password' ? '<i class="bi bi-eye-fill"></i>' : '<i class="bi bi-eye-slash-fill"></i>';
-});
